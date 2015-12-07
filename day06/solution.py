aa9f0e68f7744f8f3629ddc7a42293556f28dba3
Dario Iacampo <dario@jntstudio.net>
2015-12-07 16:21:40 +0100

    migrate day06 part2 in a different file

--- day06/solution.py
+++ day06/solution.py
@@ -78,28 +78,4 @@ with open('input.txt') as f: input = f.read().splitlines()
 for exp in input:
 	eval(exp, lights)
 lit = [x for x in lights.flatten(1) if x>0]
-print "there are %s lights lit" % len(lit)
-
-# --- Part Two ---
-
-# You just finish implementing your winning light pattern when you realize
-# you mistranslated Santa's message from Ancient Nordic Elvish.
-
-# The light grid you bought actually has individual brightness controls;
-# each light can have a brightness of zero or more. The lights all start at zero.
-
-# The phrase turn on actually means that you should increase the brightness of those lights by 1.
-
-# The phrase turn off actually means that you should decrease the brightness of those lights by 1,
-# to a minimum of zero.
-
-# The phrase toggle actually means that you should increase the brightness of those lights by 2.
-
-# What is the total brightness of all lights combined after following Santa's instructions?
-
-# For example:
-
-# turn on 0,0 through 0,0 would increase the total brightness by 1.
-# toggle 0,0 through 999,999 would increase the total brightness by 2000000.
-
-
+print "there are %s lights lit" % len(lit) # 377891
