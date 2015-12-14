d9c6fcc307f8b94974dd466af4e5502fef9f476e
Dario Iacampo <dario@jntstudio.net>
2015-12-14 21:10:33 +0100

    day14 second part

--- day14/solution.py
+++ day14/solution.py
@@ -1,4 +1,5 @@
 import re
+from collections import defaultdict
 # --- Day 14: Reindeer Olympics ---
 
 # This year is the Reindeer Olympics! Reindeer can fly at high speeds,
@@ -71,3 +72,39 @@ with open('input.txt', 'r') as f: input = f.read().splitlines()
 results = [ reindeer(2503, eval_input(x)) for x in input ]
 print results
 print max(results, key=lambda x: x[0])
+
+# --- Part Two ---
+
+# Seeing how reindeer move in bursts, Santa decides he's not pleased with the old scoring system.
+
+# Instead, at the end of each second, he awards one point to the reindeer currently in the lead.
+# (If there are multiple reindeer tied for the lead, they each get one point.)
+# He keeps the traditional 2503 second time limit, of course, as doing otherwise would be entirely ridiculous.
+
+# Given the example reindeer from above, after the first second, Dancer is in the lead and gets one point.
+# He stays in the lead until several seconds into Comet's second burst: after the 140th second, Comet pulls
+# into the lead and gets his first point. Of course, since Dancer had been in the lead
+# for the 139 seconds before that, he has accumulated 139 points by the 140th second.
+
+# After the 1000th second, Dancer has accumulated 689 points, while poor Comet,
+# our old champion, only has 312. So, with the new scoring system, Dancer would win
+# (if the race ended at 1000 seconds).
+
+# Again given the descriptions of each reindeer (in your puzzle input), after exactly 2503
+# seconds, how many points does the winning reindeer have?
+
+partecipants = [eval_input(x) for x in input]
+points = defaultdict(int)
+for s in range(1, 2503+1):
+    leaderbord = sorted( [ reindeer(s, deer) for deer in partecipants ], key=lambda x: -x[0] )
+    leader_position = leaderbord[0][0]
+
+    for leader in leaderbord:
+        km, name = leader
+        if km == leader_position:
+            points[name] += 1
+            
+print points
+
+
+
