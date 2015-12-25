18774f3905046811756489afe6a6bb522dcd4661
Dario Iacampo <dario@jntstudio.net>
2015-12-25 16:07:21 +0100

    measure number of operations in bfs

--- day19/bfs.py
+++ day19/bfs.py
@@ -4,13 +4,16 @@ def breadth_first_search(start, is_goal, successors, heuristic=lambda successor,
 	
 	frontier = collections.deque([(start, [])])
 	visited = collections.defaultdict(bool)
+	ops = 0
 
 	while len(frontier) > 0:
 		state, path = frontier.pop()
 		for s in successors(state):
 			if is_goal(s):
+				print 'operations: %s' % ops
 				return path + [s]
 			else:
 				if not visited[s] and heuristic(s, path):
+					ops += 1
 					frontier.appendleft( (s, path+[s]) )
 					visited[s] = True
