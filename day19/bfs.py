2538c1385f27afa6e583f066ec5360b830f03489
Dario Iacampo <dario@jntstudio.net>
2015-12-25 11:56:51 +0100

    heuristic evaluation of successor state

--- day19/bfs.py
+++ day19/bfs.py
@@ -1,6 +1,6 @@
 import collections
 
-def breadth_first_search(start, is_goal, successors):
+def breadth_first_search(start, is_goal, successors, heuristic=lambda successor, path : True):
 	
 	frontier = collections.deque([(start, [])])
 	visited = collections.defaultdict(bool)
@@ -11,6 +11,6 @@ def breadth_first_search(start, is_goal, successors):
 			if is_goal(s):
 				return path + [s]
 			else:
-				if not visited[s]:
+				if not visited[s] and heuristic(s, path):
 					frontier.appendleft( (s, path+[s]) )
 					visited[s] = True
