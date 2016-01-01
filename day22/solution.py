cfef9b27f1525c36a6f534fb279cf66ed9288d64
Dario Iacampo <dario@jntstudio.net>
2016-01-01 11:07:34 +0100

    refactor compose function

--- day22/solution.py
+++ day22/solution.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-import functools
 
-def compose(*functions):
-	def compose2(f, g):
-		return lambda x: f(g(x))
-	return functools.reduce(compose2, functions, lambda x: x)
+def compose_execute_all(*functions):
+	def inner(x):
+		for f in functions:
+			f(x)
+
+	return inner
 
 noop = lambda x : None
 
