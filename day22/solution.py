ea48666bcc280f8282e0c05374cb3bc06779ec2a
Dario Iacampo <dario@jntstudio.net>
2016-01-02 11:33:12 +0100

    move spells in a separate file

--- day22/solution.py
+++ day22/solution.py
@@ -1,14 +1,8 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-def compose_execute_all(*functions):
-	def inner(x):
-		for f in functions:
-			f(x)
-
-	return inner
-
-noop = lambda x : None
+from spells import Effect, TimedEffect
+from spells import magic_missile, drain, shield, poison, recharge
 
 # --- Day 22: Wizard Simulator 20XX ---
 
@@ -76,88 +70,6 @@ class SelectSpellByPredefinedOrder(object):
 		spell = self.spells.pop()
 		return spell
 
-# Effects all work the same way.
-# Effects apply at the start of both the player's turns and the boss' turns.
-# Effects are created with a timer (the number of turns they last);
-# at the start of each turn, after they apply any effect they have,
-# their timer is decreased by one. If this decreases the timer to zero, the effect ends.
-# You cannot cast a spell that would start an effect which is already active. However,
-# effects can be started on the same turn they end.
-
-class Effect(object):
-	def __init__(self, name='', cost=0, on_apply=noop):
-		self.name = name
-		self.cost = cost
-		self.on_apply = on_apply
-
-	def apply(self, game_state):
-		if type(self) is Effect:
-			game_state.wizard.mana -= self.cost
-		self.on_apply(game_state)
-
-class TimedEffect(Effect):
-	def __init__(self, timer=1, on_end=noop, on_cast=noop, **kwargs):
-		super(TimedEffect, self).__init__(**kwargs)
-		self.timer = timer
-		self.on_end = on_end
-		self.on_cast = on_cast
-
-	def apply(self, game_state):
-		super(TimedEffect, self).apply(game_state)
-		self.timer -= 1
-		print 'its timer is now %s' % self.timer
-		if self.timer <= 0:
-			game_state.spells.remove(self)
-			print '%s ends' % self.name
-			self.on_end(game_state)
-
-	def cast(self, game_state):
-		game_state.wizard.mana -= self.cost
-		self.on_cast(game_state)
-
-def damage_opponent(amount):
-	def f(game_state):
-		print 'deals %s damage;' % amount
-		game_state.boss.hit_points -= amount
-	return f
-
-def heal_me(amount):
-	def f(game_state):
-		print 'hels me %s hit points' % amount
-		game_state.wizard.hit_points+=amount
-	return f
-
-# Magic Missile costs 53 mana. It instantly does 4 damage.
-magic_missile = lambda : Effect(name='Magic Missile', on_apply=damage_opponent(4), cost=53)
-
-# Drain costs 73 mana. It instantly does 2 damage and heals you for 2 hit points.
-drain = lambda : Effect(name='Drain', cost=73, on_apply=compose_execute_all(damage_opponent(2), heal_me(2)))
-
-# Shield costs 113 mana. It starts an effect that lasts for 6 turns.
-# While it is active, your armor is increased by 7.
-def increase_armor(amount):
-	def f(game_state):
-		print 'increase armor by %s' % amount
-		game_state.wizard.armor += 7
-	return f
-
-shield = lambda : TimedEffect(name='Shield', cost=113, timer=6, on_cast=increase_armor(7), on_end=increase_armor(-7))
-
-# Poison costs 173 mana. It starts an effect that lasts for 6 turns.
-# At the start of each turn while it is active, it deals the boss 3 damage.
-
-poison = lambda : TimedEffect(name='Poison', cost=173, timer=6, on_apply=damage_opponent(3))
-
-# Recharge costs 229 mana. It starts an effect that lasts for 5 turns.
-# At the start of each turn while it is active, it gives you 101 new mana.
-
-def increase_mana(amount):
-	def f(game_state):
-		print 'provides %s mana' % amount
-		game_state.wizard.mana += amount
-	return f
-
-recharge = lambda : TimedEffect(name='Recharge', cost=229, timer=5, on_apply=increase_mana(101))
 
 class GameState(object):
 	def __init__(self, wizard, boss):
